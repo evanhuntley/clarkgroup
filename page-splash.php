3dd3ab49c202535686b1e26a303008a013fbe42b
Evan Huntley <evan.huntley@gmail.com>
2015-05-29 13:30:05 -0400

    Adding content block to splash page

--- page-splash.php
+++ page-splash.php
@@ -41,5 +41,16 @@
             </li>
         </ul>
     </div>
+    <div class="slider flexslider">
+        Slider here
+    </div>
+    <div class="content-block">
+        <h2>An Overview of Our Services</h2>
+        <div class="content">
+            <p>Duiass sed odiosdfdf kjgphj  sit amet nibh orem Ipsum. Proin cem nibh id ell gravida niamevel velit arqsdfuet. Aeneasollicitudin, loruis bibendum aip sum, necem nibh id ed odiometate cursus i am necem nibh id elltsdf el tincidng fdum auctoron  sequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulpus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus oddf dfg io tincictor a ornare od</p>
+            <p>Duiass sed odiosdfdf kjgphj  sit amet nibh orem Ipsum. Proin cem nibh id ell gravida niamevel velit arqsdfuet. Aeneasollicitudin, loruis bibendum aip sum, necem nibh id ed odiometate cursus i am necem nibh id elltsdf el tincidng fdum auctoron  sequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulpus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus oddf dfg io tincictor a ornare od</p>
+        </div>
+    </div>
+
 </section>
 <?php get_footer(); ?>
