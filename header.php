a62d6f1d203b45e80549fbe1d293dd2d34721ce6
Evan Huntley <evan.huntley@gmail.com>
2015-05-29 07:35:01 -0400

    First pass at responsive homepage

--- header.php
+++ header.php
@@ -43,8 +43,8 @@
 
 <body <?php body_class(); ?> id="top">
 		<header>
-		    <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo"><?php bloginfo( 'name' ); ?></a></h1>
-		    <button class="nav-toggle">Navigation</button>
+		    <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo"><img src="<?php echo bloginfo('template_directory'); ?>/assets/img/l_clark-group.png" /> <span><?php bloginfo( 'name' ); ?><span></a></h1>
+		    <button class="nav-toggle"><span>Navigation</span></button>
 				<nav role="navigation">
 						<?php
 								$args = array(
