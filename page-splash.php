d724e719afd640e0550a03f3c87dea6043477ec1
Evan Huntley <evan.huntley@gmail.com>
2015-05-29 13:52:15 -0400

    Adding service features to splash page

--- page-splash.php
+++ page-splash.php
@@ -41,9 +41,11 @@
             </li>
         </ul>
     </div>
-    <div class="slider flexslider">
-        Slider here
-    </div>
+</section>
+<div class="slider flexslider">
+    Slider here
+</div>
+<section class="main-content">
     <div class="content-block">
         <h2>An Overview of Our Services</h2>
         <div class="content">
@@ -51,6 +53,28 @@
             <p>Duiass sed odiosdfdf kjgphj  sit amet nibh orem Ipsum. Proin cem nibh id ell gravida niamevel velit arqsdfuet. Aeneasollicitudin, loruis bibendum aip sum, necem nibh id ed odiometate cursus i am necem nibh id elltsdf el tincidng fdum auctoron  sequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulpus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus oddf dfg io tincictor a ornare od</p>
         </div>
     </div>
-
+</section>
+<div class="service-feature">
+    <div class="content">
+        <h3>This is What a Heading Looks Like</h3>
+        <p>Duiass sed odiosdfdf kjhjt amet nibh oremumroin cem nibh id ell gravida niamvel velnean sollicitudin, lorem quis bibendum aipsum, necem nibh id elluis sed odio sit amet nibhulputate cursus i am nec cem nibh id elltellus a om aurlit co aptent taciti sociosqu ad litora torquent per conub</p>
+    </div>
+    <img src="<?php echo bloginfo('template_directory'); ?>/assets/img/bg_mission.jpg" />
+</div>
+<div class="service-feature">
+    <div class="content flip">
+        <h3>This is What a Heading Looks Like</h3>
+        <p>Duiass sed odiosdfdf kjhjt amet nibh oremumroin cem nibh id ell gravida niamvel velnean sollicitudin, lorem quis bibendum aipsum, necem nibh id elluis sed odio sit amet nibhulputate cursus i am nec cem nibh id elltellus a om aurlit co aptent taciti sociosqu ad litora torquent per conub</p>
+    </div>
+    <img class="flip" src="<?php echo bloginfo('template_directory'); ?>/assets/img/bg_mission.jpg" />
+</div>
+<section class="main-content">
+    <div class="content-block">
+        <h2>An Overview of Our Services</h2>
+        <div class="content">
+            <p>Duiass sed odiosdfdf kjgphj  sit amet nibh orem Ipsum. Proin cem nibh id ell gravida niamevel velit arqsdfuet. Aeneasollicitudin, loruis bibendum aip sum, necem nibh id ed odiometate cursus i am necem nibh id elltsdf el tincidng fdum auctoron  sequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulpus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus oddf dfg io tincictor a ornare od</p>
+            <p>Duiass sed odiosdfdf kjgphj  sit amet nibh orem Ipsum. Proin cem nibh id ell gravida niamevel velit arqsdfuet. Aeneasollicitudin, loruis bibendum aip sum, necem nibh id ed odiometate cursus i am necem nibh id elltsdf el tincidng fdum auctoron  sequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulpus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus oddf dfg io tincictor a ornare od</p>
+        </div>
+    </div>
 </section>
 <?php get_footer(); ?>
